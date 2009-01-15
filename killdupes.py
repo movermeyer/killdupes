67ca9871ac52ff5f79162cffda4d3750ad11b0cf
Martin Matusiak <numerodix@gmail.com>
2009-01-15 19:09:16 +0100

    make safe for file changes during processing should now not crash if files disappear should report files that have changed size since beginning of run

--- killdupes.py
+++ killdupes.py
@@ -21,11 +21,23 @@ import time
 CHUNK = 1024*100
 BYTES_READ = 0
 
-_units = { 0:"B", 1:"KB", 2:"MB", 3:"GB", 4:"TB", 5:"PB", 6:"EB", 7:"ZB"}
+_units = { 0:" B", 1:"KB", 2:"MB", 3:"GB", 4:"TB", 5:"PB", 6:"EB", 7:"ZB" }
+
+class File(object):
+    def __init__(self, filename):
+        self.name = filename
+        self.mtime = 0
+        self.size = 0
+        try:
+            st = os.stat(self.name)
+            self.mtime = st.st_mtime
+            self.size = st.st_size
+        except e:
+            write_out("%s\n" % e)
 
 class Record(object):
-    def __init__(self, filename, data=None, eof=False):
-        self.filename = filename
+    def __init__(self, fileobj, data=None, eof=False):
+        self.fileobj = fileobj
         self.data = data
         self.eof = eof
 
@@ -42,12 +54,20 @@ def format_size(size):
     return r.rjust(5) + " " + u.ljust(2)
 
 def format_date(date):
-    return time.strftime("%d.%m.%Y %H:%M:%S", time.gmtime(date))
+    return time.strftime("%d.%m.%y %H:%M:%S", time.gmtime(date))
 
-def format_file(filename):
-    st = os.stat(filename)
-    return ("%s  %s  %s" % 
-          (format_size(st.st_size), format_date(st.st_mtime), filename))
+def format_file(fileobj):
+    size = format_size(fileobj.size)
+    try:
+        diff = os.path.getsize(fileobj.name) - fileobj.size
+        diff_abs = abs(diff)
+        if diff < 0:
+            size += "-" + format_size(diff_abs).strip()
+        elif diff > 0:
+            size += "+" + format_size(diff_abs).strip()
+    except OSError, e:
+        write_out("%s\n" % e)
+    return ("%s  %s  %s" % (size, format_date(fileobj.mtime), fileobj.name))
 
 def write_out(s):
     sys.stdout.write(s)
@@ -60,8 +80,14 @@ def write_err(s):
 def clear_err():
     write_err(79*" "+"\r")
 
-def write_fileline(prefix, filename):
-    write_out("%s %s\n" % (prefix, format_file(filename)))
+def delete(filename):
+    try:
+        os.unlink(filename)
+    except OSError, e:
+        write_out("%s\n" % e)
+
+def write_fileline(prefix, fileobj):
+    write_out("%s %s\n" % (prefix, format_file(fileobj)))
 
 def get_hash(idx, data):
     m = hashlib.md5()
@@ -71,10 +97,10 @@ def get_hash(idx, data):
 def get_filelist(pattern=None, lst=None):
     files = []
     it = lst or glob.iglob(pattern)
-    for file in it:
-        file = file.strip()
-        if os.path.isfile(file) and not os.path.islink(file):
-            files.append(Record(file))
+    for filename in it:
+        filename = filename.strip()
+        if os.path.isfile(filename) and not os.path.islink(filename):
+            files.append(Record(File(filename)))
     return files
 
 def get_chunk(offset, length, filename):
@@ -91,17 +117,15 @@ def get_chunk(offset, length, filename):
         return 0, ""
 
 def short_name(lst):
-    lst.sort(cmp=lambda x, y: cmp((len(x), x), (len(y), y)))
+    lst.sort(cmp=lambda x, y: cmp((len(x.name), x.name), (len(y.name), y.name)))
     return lst
 
 def rev_file_size(lst):
-    lst.sort(reverse=True,
-             cmp=lambda x, y: cmp(os.path.getsize(x), os.path.getsize(y)))
+    lst.sort(reverse=True, cmp=lambda x, y: cmp(x.size, y.size))
     return lst
 
 def rec_file_size(lst):
-    lst.sort(cmp=lambda x, y: cmp(os.path.getsize(x.filename),
-                                  os.path.getsize(y.filename)))
+    lst.sort(cmp=lambda x, y: cmp(x.fileobj.size, y.fileobj.size))
     return lst
 
 def compute(pattern=None, lst=None):
@@ -125,7 +149,7 @@ def compute(pattern=None, lst=None):
             reads = []
             readsize = CHUNK
             for (rid, record) in enumerate(rs):
-                ln, data = get_chunk(offset, readsize, record.filename)
+                ln, data = get_chunk(offset, readsize, record.fileobj.name)
                 s = ("%s | Offs %s | Buck %s/%s | File %s/%s | Rs %s" % 
                       (format_size(BYTES_READ),
                        format_size(offset),
@@ -139,7 +163,7 @@ def compute(pattern=None, lst=None):
                 if ln == 0:
                     record.eof = True
                 else:
-                    r = Record(record.filename, data)
+                    r = Record(record.fileobj, data=data)
                     if ln < readsize:
                         readsize = ln
                     reads.append(r)
@@ -165,20 +189,22 @@ def compute(pattern=None, lst=None):
         offset_hashes = offsets[offset]
         for (hash, rs) in offset_hashes.items():
             if offset == 0:
-                zerosized = [r.filename for r in rs if r.eof]
+                zerosized = [r.fileobj for r in rs if r.eof]
             else:
                 if len(rs) > 1:
                     eofs = [r for r in rs if r.eof]
                     n_eofs = [r for r in rs if not r.eof]
                     if len(eofs) >= 2 and len(n_eofs) == 0:
-                        duplicates[eofs[0].filename] = [r.filename for r in eofs[1:]]
+                        duplicates[eofs[0].fileobj.name] = \
+                            [r.fileobj for r in eofs]
                     if len(eofs) >= 1 and len(n_eofs) >= 1:
-                        key = rev_file_size([r.filename for r in n_eofs])[0]
+                        largest = rev_file_size([r.fileobj for r in n_eofs])[0] 
+                        key = largest.name
                         if not key in incompletes:
-                            incompletes[key] = []
+                            incompletes[key] = [largest]
                         for r in eofs:
-                            if r.filename not in incompletes[key]:
-                                incompletes[key].append(r.filename)
+                            if r.fileobj not in incompletes[key]:
+                                incompletes[key].append(r.fileobj)
 
     return zerosized, incompletes, duplicates
 
@@ -195,35 +221,33 @@ def main(pattern=None, lst=None):
 
         if zerosized:
             write_out("Empty files:\n")
-            for f in zerosized: 
-                q_zero.append(f)
-                write_fileline(kill, f)
+            for fileobj in zerosized: 
+                q_zero.append(fileobj)
+                write_fileline(kill, fileobj)
 
         if incompletes:
             write_out("Incompletes:\n")
-            for (idx, (f, fs)) in enumerate(incompletes.items()):
-                fs.append(f)
+            for (idx, (_, fs)) in enumerate(incompletes.items()):
                 fs = rev_file_size(fs)
-                for (i, f) in enumerate(fs):
+                for (i, fileobj) in enumerate(fs):
                     prefix = keep
-                    if os.path.getsize(f) < os.path.getsize(fs[0]):
-                        q_inc.append(f)
+                    if fileobj.size < fs[0].size:
+                        q_inc.append(fileobj)
                         prefix = kill
-                    write_fileline(prefix, f)
+                    write_fileline(prefix, fileobj)
                 if idx < len(incompletes) - 1:
                     write_out('\n')
 
         if duplicates:
             write_out("Duplicates:\n")
-            for (idx, (f, fs)) in enumerate(duplicates.items()):
-                fs.append(f)
+            for (idx, (_, fs)) in enumerate(duplicates.items()):
                 fs = short_name(fs)
-                for (i, f) in enumerate(fs):
+                for (i, fileobj) in enumerate(fs):
                     prefix = keep
                     if i > 0:
-                        q_dupe.append(f)
+                        q_dupe.append(fileobj)
                         prefix = kill
-                    write_fileline(prefix, f)
+                    write_fileline(prefix, fileobj)
                 if idx < len(duplicates) - 1:
                     write_out('\n')
 
@@ -231,11 +255,11 @@ def main(pattern=None, lst=None):
         inp = raw_input()
 
         if "e" in inp or "a" in inp:
-            for f in q_zero: os.unlink(f)
+            for fileobj in q_zero: delete(fileobj.name)
         if "i" in inp or "a" in inp:
-            for f in q_inc: os.unlink(f)
+            for fileobj in q_inc: delete(fileobj.name)
         if "d" in inp or "a" in inp:
-            for f in q_dupe: os.unlink(f)
+            for fileobj in q_dupe: delete(fileobj.name)
 
 if __name__ == "__main__":
     pat = '*'
